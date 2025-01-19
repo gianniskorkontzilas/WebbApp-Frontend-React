b655bb42986fbff947e95d5996ac0988ee4046c1
gianniskorkontzilas <gianniskorkontzilas@hotmail.com>
2025-01-19 23:58:20 +0200

    wrap App component with AuthProvider and ErrorProvider

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import { AuthProvider } from "./context/authContext.tsx"; 
+import { ErrorProvider } from "./context/ErrorContext.tsx"; 
+import AppRouter from "./App.tsx"; 
+
+const App = () => {
+  return (
+    <AuthProvider> 
+      <ErrorProvider>
+        <AppRouter />
+      </ErrorProvider>
+    </AuthProvider>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
 root.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
